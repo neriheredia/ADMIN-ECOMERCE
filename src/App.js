f9620b07a77d142e7972dabb7fc7af4e68243805
neriheredia <developer.heredia@gmail.com>
2022-04-30 20:49:23 -0300

    test

--- src/App.js
+++ src/App.js
@@ -12,49 +12,48 @@ import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
 
-
 function App() {
-    // const admin = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.isAdmin
-    const user = useSelector(state => state.user.currentUser === null)
-    return (
-        <Router>
-            <Switch>
-                {user ? (
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                ) : (
-                    <>
-                        <Topbar />
-                        <div className="container">
-                            <Sidebar />
-                            <Route exact path="/">
-                                <Home />
-                            </Route>
-                            <Route path="/users">
-                                <UserList />
-                            </Route>
-                            <Route path="/user/:userId">
-                                <User />
-                            </Route>
-                            <Route path="/newUser">
-                                <NewUser />
-                            </Route>
-                            <Route path="/products">
-                                <ProductList />
-                            </Route>
-                            <Route path="/product/:productId">
-                                <Product />
-                            </Route>
-                            <Route path="/newproduct">
-                                <NewProduct />
-                            </Route>
-                        </div>
-                    </>
-                )}
-            </Switch>
-        </Router>
-    );
+  // const admin = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser.isAdmin
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Switch>
+        {!user ? (
+          <Route path="/login">
+            <Login />
+          </Route>
+        ) : (
+          <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/products">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newproduct">
+                <NewProduct />
+              </Route>
+            </div>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
